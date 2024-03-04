1f06370fe6f9e4773aea55792fc64accb015c92b
AlanAOliveira <alanalves123456789@gmail.com>
2024-03-04 14:10:34 -0300

    Create 1162.c

---
+++ 1162.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int N, L, OE, OS, i;
+    
+    scanf("%i", &N);
+    int S[N];
+    
+    for(i=0; i<N; i++){
+       scanf("%i", &L);
+       
+       for(i=0;i<L;i++)
+            
+    }   
+        
+    
+ 
+    return 0;
+}
