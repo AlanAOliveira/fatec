ac87a9b5f96a149043faffd075e880c89ccdf4c3
AlanAOliveira <alanalves123456789@gmail.com>
2024-03-06 08:57:33 -0300

    Update 1162.c

--- 1162.c
+++ 1162.c
@@ -2,22 +2,30 @@
  
 int main() {
  
-    int N, L, i, j, k;
+    int N, L, i, j, k, Swap=0;
     
     scanf("%i", &N);    
     int S[N];
     
     for(i=0; i<N; i++){
         scanf("%i", &L);
-        int OE[l], OS[l];
+        int OE[L], OS[L];
         
         for(j=0;j<L;j++){
             scanf(" %i",&OE[j]);
             OS[j] = OE[j];
         }
         
-        for(j=0;j<L;j++)
-            while()
- 
+        for(j=0;j<L;j++){
+            for(k=0;k<L;k++)
+                if(OE[j] < OS[k]){
+                    OS[k] = OE[j];
+                    Swap++;
+                }
+            S[j] = Swap;
+        }
+        for(j=0;j<L;j++){
+            printf("Optimal train swapping takes %i swaps.\n", S[j]);
+        }   
     }return 0;
 }
